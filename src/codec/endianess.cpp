3cdbe400d9247507bd6cc1fde95a25d6d2fb9501
hankedan000 <dany32412@gmail.com>
2021-02-18 06:20:01 -0500

    Adds *_to_native endian swap functions

--- src/codec/endianess.cpp
+++ src/codec/endianess.cpp
@@ -15,6 +15,8 @@ namespace endian
 		return big_int.c[0] == 1; 
 	}
 
+	// ---------------------------------------
+
 	template<>
 	uint8_t
 	native_to_big(
@@ -103,6 +105,98 @@ namespace endian
 		return bswap_64(val);
 	}
 
+	// ---------------------------------------
+
+	template<>
+	uint8_t
+	big_to_native(
+		uint8_t val)
+	{
+		return val;
+	}
+
+	template<>
+	int8_t
+	big_to_native(
+		int8_t val)
+	{
+		return val;
+	}
+
+	template<>
+	uint16_t
+	big_to_native(
+		uint16_t val)
+	{
+		if (is_big_endian())
+		{
+			return val;
+		}
+		return bswap_16(val);
+	}
+
+	template<>
+	int16_t
+	big_to_native(
+		int16_t val)
+	{
+		if (is_big_endian())
+		{
+			return val;
+		}
+		return bswap_16(val);
+	}
+
+	template<>
+	uint32_t
+	big_to_native(
+		uint32_t val)
+	{
+		if (is_big_endian())
+		{
+			return val;
+		}
+		return bswap_32(val);
+	}
+
+	template<>
+	int32_t
+	big_to_native(
+		int32_t val)
+	{
+		if (is_big_endian())
+		{
+			return val;
+		}
+		return bswap_32(val);
+	}
+
+	template<>
+	uint64_t
+	big_to_native(
+		uint64_t val)
+	{
+		if (is_big_endian())
+		{
+			return val;
+		}
+		return bswap_64(val);
+	}
+
+	template<>
+	int64_t
+	big_to_native(
+		int64_t val)
+	{
+		if (is_big_endian())
+		{
+			return val;
+		}
+		return bswap_64(val);
+	}
+
+	// ---------------------------------------
+
 	template<>
 	uint8_t
 	native_to_little(
@@ -190,4 +284,95 @@ namespace endian
 		}
 		return val;
 	}
+
+	// ---------------------------------------
+
+	template<>
+	uint8_t
+	little_to_native(
+		uint8_t val)
+	{
+		return val;
+	}
+
+	template<>
+	int8_t
+	little_to_native(
+		int8_t val)
+	{
+		return val;
+	}
+
+	template<>
+	uint16_t
+	little_to_native(
+		uint16_t val)
+	{
+		if (is_big_endian())
+		{
+			return bswap_16(val);
+		}
+		return val;
+	}
+
+	template<>
+	int16_t
+	little_to_native(
+		int16_t val)
+	{
+		if (is_big_endian())
+		{
+			return bswap_16(val);
+		}
+		return val;
+	}
+
+	template<>
+	uint32_t
+	little_to_native(
+		uint32_t val)
+	{
+		if (is_big_endian())
+		{
+			return bswap_32(val);
+		}
+		return val;
+	}
+
+	template<>
+	int32_t
+	little_to_native(
+		int32_t val)
+	{
+		if (is_big_endian())
+		{
+			return bswap_32(val);
+		}
+		return val;
+	}
+
+	template<>
+	uint64_t
+	little_to_native(
+		uint64_t val)
+	{
+		if (is_big_endian())
+		{
+			return bswap_64(val);
+		}
+		return val;
+	}
+
+	template<>
+	int64_t
+	little_to_native(
+		int64_t val)
+	{
+		if (is_big_endian())
+		{
+			return bswap_64(val);
+		}
+		return val;
+	}
+
 }
--- src/codec/endianess.h
+++ src/codec/endianess.h
@@ -12,10 +12,20 @@ namespace endian
 	T
 	native_to_big(
 		T val);
+	
+	template<typename T>
+	T
+	big_to_native(
+		T val);
 
 	template<typename T>
 	T
 	native_to_little(
+		T val);
+
+	template<typename T>
+	T
+	little_to_native(
 		T val);
 }
 
