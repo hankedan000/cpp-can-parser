e6ba4fc41cce7c5cd6d9818d9d99098d8620a092
hankedan000 <dany32412@gmail.com>
2021-02-24 08:15:33 -0500

    Adds test-decode assertion statement

--- tests/test-decode.cpp
+++ tests/test-decode.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
 #include "cpp-can-parser/CANDatabase.h"
+#include <exception>
+
+#define ASSERT_EQUAL(expect,actual) \
+    if (expect != actual) { \
+        std::string error("ASSERT_EQUAL!\n"); \
+        error += std::string("expect: ") + #expect + " (" + std::to_string(expect) + ")\n"; \
+        error += std::string("actual: ") + #actual + " (" + std::to_string(actual) + ")\n"; \
+        throw std::runtime_error(error); \
+    }
 
 int main(int argc, char** argv) {
     using namespace CppCAN;
@@ -8,24 +17,19 @@ int main(int argc, char** argv) {
 
     try {
         CANDatabase db = CANDatabase::fromFile(file);
-        CANFrame frame = db[1520];
-        uint64_t data = 0x023B000000000000;
-        // uint64_t data = 0xFFFFFFFFFFFFFFFF;
-        for (const auto &signal_itr : frame)
-        {
-            const CANSignal &sig = signal_itr.second;
-            CANSignal::raw_t raw = sig.decode(&data);
-            printf(
-                "%s: scale = %f, offset = %f, raw = %lu, phys = %f\n",
-                sig.name().c_str(),
-                sig.scale(),
-                sig.offset(),
-                raw,
-                sig.rawToPhys(raw));
-        }
+        CANFrame frame = db[1523];
+        uint64_t data = 0x4A004A006E002600;
+        const CANSignal &batt_sig = frame["batt"];
+        CANSignal::raw_t batt_raw = batt_sig.decode(&data);
+        ASSERT_EQUAL(0x6e,batt_raw);
+        ASSERT_EQUAL(11.0,batt_sig.rawToPhys(batt_raw));
     }
     catch(const CANDatabaseException& e) {
         std::cerr << "Error with file \"" << file << "\": " << e.what() << std::endl;
+        okay = false;
+    }
+    catch(const std::runtime_error& re) {
+        std::cerr << re.what() << std::endl;
         okay = false;
     }
     catch(...) {
